f9b50b244be54ce5e92c6725d56c2193f58344c9
Vivek Yadav <vivektalent200@gmail.com>
2024-11-29 22:54:03 +0530

    ALL DONE

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,20 +7,20 @@ export default function Navbar() {
             <div className="max-w-7xl mx-auto flex justify-center items-center">
                 {/* Social Media Links with Fade-In Animation */}
                 <div className="flex justify-center gap-6 animate__animated animate__fadeIn animate__delay-0.9s">
-                    <a href="https://github.com/ez-vivek" className="text-white hover:text-gray-400 transition-colors">
+                    <a href="https://github.com/ez-vivek" target="_blank" className="text-white hover:text-gray-400 transition-colors">
                         <i className="bi bi-github text-2xl"></i>
                     </a>
-                    <a href="https://www.linkedin.com/in/ezvivek" className="text-white hover:text-gray-400 transition-colors">
+                    <a href="https://www.linkedin.com/in/ezvivek"target="_blank" className="text-white hover:text-gray-400 transition-colors">
                         <i className="bi bi-linkedin text-2xl"></i>
                     </a>
-                    <a href="https://www.instagram.com/ez.vivek" className="text-white hover:text-gray-400 transition-colors">
+                    <a href="https://www.instagram.com/ez.vivek"target="_blank" className="text-white hover:text-gray-400 transition-colors">
                         <i className="bi bi-instagram text-2xl"></i>
                     </a>
-                    <a href="https://x.com/ez_vivek" className="text-white hover:text-gray-400 transition-colors">
-                        <i className="bi bi-twitter-x text-2xl"></i> {/* X icon */}
+                    <a href="https://x.com/ez_vivek"target="_blank" className="text-white hover:text-gray-400 transition-colors">
+                        <i className="bi bi-twitter-x text-2xl"></i>
                     </a>
-                    <a href="mailto:your-email@example.com" className="text-white hover:text-gray-400 transition-colors">
-                        <i className="bi bi-envelope text-2xl"></i> {/* Mail icon */}
+                    <a href="mailto:vivektalent200@gmail.com" className="text-white hover:text-gray-400 transition-colors">
+                        <i className="bi bi-envelope text-2xl"></i>
                     </a>
                 </div>
             </div>
