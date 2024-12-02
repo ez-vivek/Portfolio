c41090d3220706c4a6be664c907311846ee3a931
Vivek Yadav <vivektalent200@gmail.com>
2024-12-02 20:19:21 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,3 @@
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import Services from './components/Services';
-// import Education from './components/Education';
-// import Projects from './components/Projects';
-// import Process from './components/Process';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
-
-// function App() {
-//   return (
-//     <div className="bg-black min-h-screen text-white">
-//       <Navbar />
-//       <Hero />
-//       <Services />
-//       <Education/>
-//       <Projects />
-//       <Process />
-//       <Contact/>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import useLocomotiveScroll from './hooks/useLocomotiveScroll';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
@@ -35,20 +8,16 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
-  const scrollRef = useLocomotiveScroll();
-
   return (
     <div className="bg-black min-h-screen text-white">
       <Navbar />
-      <div ref={scrollRef} data-scroll-container>
-        <Hero />
-        <Services />
-        <Education/>
-        <Projects />
-        <Process />
-        <Contact/>
-        <Footer />
-      </div>
+      <Hero />
+      <Services />
+      <Education/>
+      <Projects />
+      <Process />
+      <Contact/>
+      <Footer />
     </div>
   );
 }
