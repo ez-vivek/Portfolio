8698232366c6de11a2c8918b4ff60b957a54718d
Vivek Yadav <vivektalent200@gmail.com>
2024-12-03 20:09:59 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Services from './components/Services';
 import Education from './components/Education';
 import Projects from './components/Projects';
+import Services from './components/Services';
 import Process from './components/Process';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -11,10 +11,10 @@ function App() {
   return (
     <div className="bg-black min-h-screen text-white">
       <Navbar />
-      <Hero />
-      <Services />
+      <Hero />     
       <Education/>
       <Projects />
+      <Services />
       <Process />
       <Contact/>
       <Footer />
